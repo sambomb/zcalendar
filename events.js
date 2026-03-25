4413ffc70aa7be19f074f543918bad7d57aa4703
Samir Sardinha <sssabreu@outlook.com>
2026-03-25 11:21:54 -0300

    Initialize events and icons in events.js
    Added a list of events and associated icons.

---
+++ events.js
@@ -0,0 +1,13 @@
+const EVENTS=[["Mod Vehicle Boost","Shelter Upgrade","Army Expansion","Age of Science","Hero Initiative","Army Expansion"],
+["Shelter Upgrade","Age of Science","Mod Vehicle Boost","Hero Initiative","Army Expansion","Mod Vehicle Boost"],
+["Age of Science","Hero Initiative","Shelter Upgrade","Army Expansion","Mod Vehicle Boost","Shelter Upgrade"],
+["Hero Initiative","Army Expansion","Age of Science","Mod Vehicle Boost","Shelter Upgrade","Age of Science"],
+["Army Expansion","Mod Vehicle Boost","Hero Initiative","Shelter Upgrade","Age of Science","Hero Initiative"],
+["Mod Vehicle Boost","Shelter Upgrade","Army Expansion","Age of Science","Hero Initiative","Army Expansion"],
+["Age of Science","Mod Vehicle Boost","Hero Initiative","Shelter Upgrade","Army Expansion","Hero Initiative"]]
+
+const ICONS={
+white:"white.png",
+red:"red.png",
+gold:"gold.png"
+}
